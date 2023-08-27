995ad6e0f1ec7140268a9a4e6a84d4f94e81bced
Jorgetorreslopez <usda4ever@gmail.com>
2023-08-27 16:13:09 -0400

    Created Welcome Button On Homepage

--- src/Pages/HomePage/index.js
+++ src/Pages/HomePage/index.js
@@ -1,11 +1,23 @@
-import React from 'react';
-import styles from './HomePage.module.css'; 
+import React from "react";
+import styles from "./HomePage.module.css";
 
 const HomePage = () => {
   return (
     <div className={styles.container}>
       <div className={styles.welcomeText}>
-        Welcome to my portfolio!
+        HEY, ARE YOU TRAVELING TOO?
+        <br />
+        <button className={styles.btn} type="button">
+          <strong className={styles.strong}>Join the Journey</strong>
+          <div id={styles.containerStars}>
+            <div id="stars"></div>
+          </div>
+
+          <div id="glow">
+            <div className={styles.circle}></div>
+            <div className={styles.circle}></div>
+          </div>
+        </button>
       </div>
     </div>
   );
