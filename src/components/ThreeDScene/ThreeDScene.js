8e3714820f8ef883280b59a0c5f5a69228a8fbec
Jorgetorreslopez <usda4ever@gmail.com>
2023-09-17 13:04:04 -0400

    Fixed Home Page 3d Object

--- src/components/ThreeDScene/ThreeDScene.js
+++ src/components/ThreeDScene/ThreeDScene.js
@@ -1,18 +1,57 @@
-// components/ThreeDScene.js
-import React, { useRef } from 'react';
-import { Canvas } from 'react-three-fiber';
-import { OrbitControls, PerspectiveCamera, Box } from '@react-three/drei';
-import '../../app/globals.css'
-
+import React, { useRef } from "react";
+import { useRouter } from "next/router";
+import { Canvas, useFrame } from "@react-three/fiber";
+import {
+  OrbitControls,
+  PerspectiveCamera,
+  Box,
+  Html,
+  Group,
+} from "@react-three/drei";
+import styles from "./ThreeD.module.css";
 
 function ThreeDScene() {
   const cameraRef = useRef();
+  const groupRef = useRef();
+
+  const router = useRouter();
+  const handleJoinJourneyClick = () => {
+    router.push("/MainPage/MainPage");
+  };
 
   return (
     <Canvas>
       <PerspectiveCamera makeDefault ref={cameraRef} position={[0, 0, 5]} />
       <OrbitControls camera={cameraRef.current} enableZoom={false} />
-      <Box args={[1, 1, 1]} position={[0, 0, 0]} />
+
+      <group ref={groupRef}>
+        <Box args={[1, 1, 1]} position={[0, 0, 0]} />
+        <Html position={[0, 0, 0]}>
+          <div className={styles.welcomeContainer}>
+            <div className={styles.welcomeText}>
+              HEY, ARE YOU TRAVELING TOO?
+              <br />
+              <div id="button-container">
+                <button
+                  className={styles.btn}
+                  type="button"
+                  onClick={handleJoinJourneyClick}
+                >
+                  <strong className={styles.strong}>Join the Journey</strong>
+                  <div id={styles.containerStars}>
+                    <div id="stars"></div>
+                  </div>
+
+                  <div id="glow">
+                    <div className={styles.circle}></div>
+                    <div className={styles.circle}></div>
+                  </div>
+                </button>
+              </div>
+            </div>
+          </div>
+        </Html>
+      </group>
     </Canvas>
   );
 }
